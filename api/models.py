90126cf884731f795c113cc8800de08ac8c341ca
itzani18 <98737694+itzani18@users.noreply.github.com>
2025-11-14 23:04:05 +0530

    Change email and location fields to CharField
    Updated email and location fields to increase maximum length.

--- api/models.py
+++ api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Inquiry(models.Model):
     name = models.CharField(max_length=120)
     phone = models.CharField(max_length=20)
-    email = models.EmailField(blank=True, null=True)
-    location = models.CharField(max_length=120, blank=True, null=True)
+    email = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
     message = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
