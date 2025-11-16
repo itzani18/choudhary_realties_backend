12c1d50e7d329e886fc1513b850f514ec409cce7
itzani18 <98737694+itzani18@users.noreply.github.com>
2025-11-16 23:02:11 +0530

    Update views.py

--- realestate_app/views.py
+++ realestate_app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db.models import Q
 from django.conf import settings
+from django.http import JsonResponse
 
 from .models import Property, PropertyImage
 from api.models import Inquiry
