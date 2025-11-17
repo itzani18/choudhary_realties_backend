6372e5edb42a758ff944a8766e71c11cc83f1016
itzani18 <98737694+itzani18@users.noreply.github.com>
2025-11-17 17:44:09 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("health/", views.health, name="health"),
 ]
