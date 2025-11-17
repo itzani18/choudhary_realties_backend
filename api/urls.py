bc355aa97662e4e9c9af599500a4203899c49343
itzani18 <98737694+itzani18@users.noreply.github.com>
2025-11-17 17:45:14 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 # api/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PropertyViewSet, InquiryViewSet, PropertyImageViewSet
+from .views import PropertyViewSet, InquiryViewSet, PropertyImageViewSet, health
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
