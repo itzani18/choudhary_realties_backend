a6b51dbe4785297cdd6a739a7f9cbc5b992391ef
itzani18 <98737694+itzani18@users.noreply.github.com>
2025-11-20 12:52:33 +0530

    Add IsAuthenticated permission to views

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser, FormParser
 
